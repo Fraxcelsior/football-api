18572b8597acd70295d044764b6d4bef1fc40564
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-26 15:28:53 +0200

    added post endpoint

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const db = require('./db')
 const model = require('./team/model')
 const teamRouter = require('./team/router')
-
+const jsonParser = bodyParser.json()
 
 //this is the API server
 const app = express()
 
+app.use(jsonParser)
 app.use(teamRouter)
 
 const port = process.env.PORT || 4000
--- team/router.js
+++ team/router.js
@@ -12,4 +12,11 @@ router.get('/team', function (req, res, next) {
         .catch(error => next(error))
 })
 
+router.post('/team', function (req, res) {
+    Team
+    .create(req.body)
+    .then(team => {res.status(201).json(team)})
+    .catch(error => next(error))
+})
+
 module.exports = router
