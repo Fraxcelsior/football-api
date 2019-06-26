058a994f437bccb9f5295b650a358fd94760d2f7
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-26 13:10:37 +0200

    slight edit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const sequelize = require('./db')
+const database = require('./db')
 
 //this is the API server
 const app = express()
+
+database
 
 const port = process.env.PORT || 4000
 
