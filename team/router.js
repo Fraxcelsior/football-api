62b5cc142ba36698de743db9605a9726401dabc1
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-26 16:02:57 +0200

    added put endpoint

--- team/router.js
+++ team/router.js
@@ -13,18 +13,30 @@ router.get('/team', function (req, res, next) {
 })
 
 router.get('/team/:id', function (req, res, next) {
-    const id =req.params.id
+    const id = req.params.id
     Team
         .findByPk(id)
         .then(team => res.status(200).json(team))
         .catch(error => next(error))
 })
 
-router.post('/team', function (req, res) {
+router.post('/team', function (req, res, next) {
     Team
-    .create(req.body)
-    .then(team => {res.status(201).json(team)})
-    .catch(error => next(error))
+        .create(req.body)
+        .then(team => { res.status(201).json(team) })
+        .catch(error => next(error))
 })
+
+router.put('./team/:id', function (req, res, next) {
+    const id = req.params.id
+    Team
+        .findByPk(id)
+        .then(team => {
+            team.update(req.body)
+        })
+        .then(team => res.status(201).json(team))
+        .catch(error => next(error))
+})
+
 
 module.exports = router
