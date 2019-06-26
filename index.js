284218066654e27ae6706742468514361aebd2b5
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-26 12:14:44 +0200

    added script start

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express')
 //this is the API server
 const app = express()
 
-const port = 4000
+const port = process.env.PORT || 4000
 
-app.listen(port, () => `API server is listening on port ${port}`)
+app.listen(port, () => console.log(`API server is listening on port ${port}`))
