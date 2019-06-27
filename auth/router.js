08456dfdad6eb5158419e41cb0a97025c31ddf62
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-27 12:24:52 +0200

    moved authentication to middleware

--- auth/router.js
+++ auth/router.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt')
 const { toJWT } = require('./wt')
 const { toData } = require('./wt')
 const User = require('../user/model')
+const auth = require('./middleware')
 const router = new Router()
 
 
@@ -52,26 +53,10 @@ router.post('/logins', function (req, res, next) {
     }
 })
 
-router.get('/secret-endpoint', (req, res) => {
-    const auth = req.headers.authorization && req.headers.authorization.split(' ')
-    if (auth && auth[0] === 'Bearer' && auth[1]) {
-        try {
-            const data = toData(auth[1])
-            res.send({
-                message: 'Thanks for visiting the secret endpoint.',
-                data
-            })
-        }
-        catch (error) {
-            res.status(400).send({
-                message: `Error ${error.name}: ${error.message}`,
-            })
-        }
-    }
-    else
-        res.status(401).send({
-            message: 'Please supply some valid credentials'
-        })
+router.get('/secret-endpoint', auth, (req, res) => {
+    res.send({
+        message: `Thanks for visiting the secret endpoint, ${req.user.email}.`,
+    })
 })
 
 
