282a8f2e2067732a9fecb03a630424cf688be2ac
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-27 11:43:19 +0200

    rewrote login code

--- auth/router.js
+++ auth/router.js
@@ -5,13 +5,49 @@ const router = new Router()
 
 
 router.post('/logins', function (req, res, next) {
-    if (!req.body) {
+    const emails = req.body.emails
+    const password = req.body.password
+
+    if (!emails || !password) {
         res.status(400).send({
             message: 'Please supply a valid email and password'
         })
-    } else res.send({
-        jwt: toJWT({ userId: 1 })
-    })
+    }
+    else {
+        //1. find user based on email, 'entity' is callback from promis
+        User
+            .findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+            .then(entity => {
+                if (!entity) {
+                    res.status(400).send({
+                        message: 'User with that email does not exist'
+                    })
+                }
+                //2. use bcrypt.compareSync to check pw against stored hash
+                if (bcrypt.compareSync(req.body.password, entity.password)) {
+                    //3. if pw is correct, return JWT with userId of user (user.id)
+                    releaseEvents.send({
+                        jwt: toJWT({ userId: entity.id })
+                    })
+                }
+                else {
+                    res.status(400).send({
+                        message: 'Password was incorrect'
+                    })
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                res.status(500).send({
+                    message: 'Oops! Something went wrong'
+                })
+            })
+        //to implement steps 1-3, we used Sequelize and promise chaining^
+    }
 })
 
 router.get('/secret-endpoint', (req, res) => {
@@ -30,11 +66,10 @@ router.get('/secret-endpoint', (req, res) => {
             })
         }
     }
-    else {
+    else
         res.status(401).send({
             message: 'Please supply some valid credentials'
         })
-    }
 })
 
 
--- user/router.js
+++ user/router.js
@@ -1,11 +1,34 @@
 const { Router } = require('express')
+const bcrypt = require('bcrypt')
 const User = require('./model')
 
 const router = new Router()
 
-router.post('/user/', function (req, res, next) {
+router.get('/user/', function (req, res, next) {
     User
-        .create(req.body)
+        .findAll()
+        .then(users => {
+            res.status(201).json({ users })
+        })
+        .catch(error => next(error))
+})
+
+router.get('/user/:id', function (req, res, next) {
+    const id = req.params.id
+    User
+        .findByPk(id)
+        .then(user => res.status(200).json(user))
+        .catch(error => next(error))
+})
+
+
+router.post('/user/', function (req, res, next) {
+    const user = {
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10)
+    }
+    User
+        .create(user)
         .then(user => { res.status(201).send(user) })
         .catch(error => next(error))
 })
